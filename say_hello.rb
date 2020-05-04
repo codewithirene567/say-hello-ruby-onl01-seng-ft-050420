7d9ae54e410a24b1b09f148f12d255fa56ec6660
Irene Scott <irenejinscott@gmail.com>
2020-05-04 20:03:08 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,18 @@
 # Build your say_hello method here
+
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+def tip_calculator(price, tip=0.2)
+  tip_amount = price * tip
+  total = price + tip_amount
+  
+  puts "The tip amount is #{tip_amount} and the total is #{total}"
+end
+
+##This is what the test will run
+
+tip_calculator(60, 0.15)
+tip_calculator(60)
+tip_calculator(0.3)
